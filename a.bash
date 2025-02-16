7e929fffffb7a6dedd335f46f40114c0391faacf
Ryubin Kim <martinok1103@naver.com>
2025-02-16 07:34:00 +0000

    add spies

--- a.bash
+++ a.bash
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-for file in [0-9][0-9]; do
+for file in [0-9][0-9] [0-9][0-9][0-9]; do
     if [[ -f "$file" ]]; then
-        mv "$file" "$file.in"
+        if [[ "$file" =~ ^[0-9]{2}$ ]]; then
+            mv "$file" "$file.in"
+        elif [[ "$file" =~ ^[0-9]{3}$ ]]; then
+            mv "$file" "$file.in"
+        fi
     fi
 done
 
-for file in [0-9][0-9].a; do
+for file in [0-9][0-9].a [0-9][0-9][0-9].a; do
     if [[ -f "$file" ]]; then
-        mv "$file" "${file%.a}.out"
+        if [[ "$file" =~ ^[0-9]{2}\.a$ ]]; then
+            mv "$file" "${file%.a}.out"
+        elif [[ "$file" =~ ^[0-9]{3}\.a$ ]]; then
+            mv "$file" "${file%.a}.out"
+        fi
     fi
 done
